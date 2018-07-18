1c6350d0667b24dcfbb2aeb7a9ff5c5243f1432e
Patrick Stanford <PStanford@janison.com.au>
2018-07-18 15:58:32 +1000

    Improve API by exporting functions rather than a class

--- src/seven-zip.ts
+++ src/seven-zip.ts
@@ -10,134 +10,129 @@ export interface SevenZipOptions {
     overwriteMode?: SevenZipOverwriteOption;
 }
 
-export class SevenZip {
-    public static sevenZipExecutableLocation: string = path.join(__dirname, '../');
+export const sevenZipExecutableLocation: string = path.join(__dirname, '../');
 
-    private static readonly optionDictionary: { [key: string]: SevenZipSwitch } = {
-        outputDirectory: SevenZipSwitch.OutputDirectory,
-        password: SevenZipSwitch.Password
-    };
+const optionDictionary: { [key: string]: SevenZipSwitch } = {
+    outputDirectory: SevenZipSwitch.OutputDirectory,
+    password: SevenZipSwitch.Password
+};
 
-    public static add(archive: string, args: SevenZipArgument[] = []) {
-        return SevenZip.exec(SevenZipCommand.Add, archive, args);
+export function add(archive: string, args: SevenZipArgument[] = []) {
+    return exec(SevenZipCommand.Add, archive, args);
+}
+
+export function benchmark(archive: string, args: SevenZipArgument[] = []) {
+    return exec(SevenZipCommand.Benchmark, archive, args);
+}
+
+function del(archive: string, args: SevenZipArgument[] = []) {
+    return exec(SevenZipCommand.Delete, archive, args);
+}
+exports.delete = del;
+
+export function extract(archive: string, fullPaths: boolean = false, args: SevenZipArgument[] | SevenZipOptions = []) {
+    return exec(fullPaths ? SevenZipCommand.ExtractWithFullPaths : SevenZipCommand.Extract, archive, args);
+}
+
+export function hash(archive: string, args: SevenZipArgument[] = []) {
+    return exec(SevenZipCommand.Hash, archive, args);
+}
+
+export function info(archive: string, args: SevenZipArgument[] = []) {
+    return exec(SevenZipCommand.Information, archive, args);
+}
+
+export function list(archive: string, args: SevenZipArgument[] = []) {
+    return exec(SevenZipCommand.List, archive, args);
+}
+
+export function rename(archive: string, args: SevenZipArgument[] = []) {
+    return exec(SevenZipCommand.Rename, archive, args);
+}
+
+export function test(archive: string, args: SevenZipArgument[] = []) {
+    return exec(SevenZipCommand.Test, archive, args);
+}
+
+export function update(archive: string, args: SevenZipArgument[] = []) {
+    return exec(SevenZipCommand.Update, archive, args);
+}
+
+/**
+ * Executes a 7zip command.
+ *
+ * Based off: https://sevenzip.osdn.jp/chm/cmdline/syntax.htm
+ *
+ * @param command
+ * @param archive
+ * @param switches
+ * @param args
+ */
+export function exec(
+    command: SevenZipCommand,
+    archive: string,
+    args: SevenZipArgument[] | SevenZipOptions = []
+    // switches: SevenZipSwitch[] = [],
+) {
+    // const stringifySwitches = SevenZip.stringifySwitches(switches);
+
+    if (!archive || !fs.existsSync(archive)) {
+        throw new Error(`Cannot find file "${archive}"`);
     }
 
-    public static benchmark(archive: string, args: SevenZipArgument[] = []) {
-        return SevenZip.exec(SevenZipCommand.Benchmark, archive, args);
-    }
+    const parsedArguments = Array.isArray(args) ? stringifyArguments(args) : parseOptions(args);
 
-    public static delete(archive: string, args: SevenZipArgument[] = []) {
-        return SevenZip.exec(SevenZipCommand.Delete, archive, args);
-    }
+    return child_process.execSync(`7za ${command} "${archive}" ${parsedArguments}`, {
+        cwd: sevenZipExecutableLocation
+    });
+}
 
-    public static extract(
-        archive: string,
-        fullPaths: boolean = false,
-        args: SevenZipArgument[] | SevenZipOptions = []
-    ) {
-        return SevenZip.exec(fullPaths ? SevenZipCommand.ExtractWithFullPaths : SevenZipCommand.Extract, archive, args);
-    }
+function stringifySwitch(sevenZipSwitch: SevenZipSwitch | SevenZipSwitchWithOption): string {
+    return typeof sevenZipSwitch === 'string' ? sevenZipSwitch : `${sevenZipSwitch.type}"${sevenZipSwitch.option}"`;
+}
 
-    public static hash(archive: string, args: SevenZipArgument[] = []) {
-        return SevenZip.exec(SevenZipCommand.Hash, archive, args);
-    }
+function stringifySwitches(switches: SevenZipSwitch[]): string {
+    const result: string[] = [];
 
-    public static info(archive: string, args: SevenZipArgument[] = []) {
-        return SevenZip.exec(SevenZipCommand.Information, archive, args);
-    }
+    switches.forEach(sevenZipSwitch => result.push(stringifySwitch(sevenZipSwitch)));
 
-    public static list(archive: string, args: SevenZipArgument[] = []) {
-        return SevenZip.exec(SevenZipCommand.List, archive, args);
-    }
+    return result.join(' ');
+}
 
-    public static rename(archive: string, args: SevenZipArgument[] = []) {
-        return SevenZip.exec(SevenZipCommand.Rename, archive, args);
-    }
+function stringifyArguments(args: SevenZipArgument[]): string {
+    const result: string[] = [];
 
-    public static test(archive: string, args: SevenZipArgument[] = []) {
-        return SevenZip.exec(SevenZipCommand.Test, archive, args);
-    }
+    args.forEach(sevenZipArgument => result.push(stringifySwitch(<SevenZipSwitch>sevenZipArgument)));
 
-    public static update(archive: string, args: SevenZipArgument[] = []) {
-        return SevenZip.exec(SevenZipCommand.Update, archive, args);
-    }
+    return result.join(' ');
+}
 
-    /**
-     * Executes a 7zip command.
-     *
-     * Based off: https://sevenzip.osdn.jp/chm/cmdline/syntax.htm
-     *
-     * @param command
-     * @param archive
-     * @param switches
-     * @param args
-     */
-    public static exec(
-        command: SevenZipCommand,
-        archive: string,
-        args: SevenZipArgument[] | SevenZipOptions = []
-        // switches: SevenZipSwitch[] = [],
-    ) {
-        // const stringifySwitches = SevenZip.stringifySwitches(switches);
+function parseOptions(options: SevenZipOptions): string {
+    const result: string[] = [];
 
-        if (!archive || !fs.existsSync(archive)) {
-            throw new Error(`Cannot find file "${archive}"`);
+    Object.keys(options).forEach(k => {
+        const key: keyof SevenZipOptions = <keyof SevenZipOptions>k;
+        const option = options[key];
+        let sevenZipSwitch: SevenZipSwitch | SevenZipSwitchWithOption | undefined;
+
+        switch (key) {
+            case 'overwriteMode':
+                sevenZipSwitch = <SevenZipOverwriteOption>option;
+                break;
+
+            default:
+                if (options[<keyof SevenZipOptions>key]) {
+                    sevenZipSwitch = <SevenZipSwitchWithOption>{
+                        type: optionDictionary[key],
+                        option: options[<keyof SevenZipOptions>key]
+                    };
+                }
         }
 
-        const stringifyArguments = Array.isArray(args)
-            ? SevenZip.stringifyArguments(args)
-            : SevenZip.parseOptions(args);
+        if (sevenZipSwitch) {
+            result.push(stringifySwitch(sevenZipSwitch));
+        }
+    });
 
-        return child_process.execSync(`7za ${command} "${archive}" ${stringifyArguments}`);
-    }
-
-    private static stringifySwitch(sevenZipSwitch: SevenZipSwitch | SevenZipSwitchWithOption): string {
-        return typeof sevenZipSwitch === 'string' ? sevenZipSwitch : `${sevenZipSwitch.type}"${sevenZipSwitch.option}"`;
-    }
-
-    private static stringifySwitches(switches: SevenZipSwitch[]): string {
-        const result: string[] = [];
-
-        switches.forEach(sevenZipSwitch => result.push(SevenZip.stringifySwitch(sevenZipSwitch)));
-
-        return result.join(' ');
-    }
-
-    private static stringifyArguments(args: SevenZipArgument[]): string {
-        const result: string[] = [];
-
-        args.forEach(sevenZipArgument => result.push(SevenZip.stringifySwitch(<SevenZipSwitch>sevenZipArgument)));
-
-        return result.join(' ');
-    }
-
-    private static parseOptions(options: SevenZipOptions): string {
-        const result: string[] = [];
-
-        Object.keys(options).forEach(k => {
-            const key: keyof SevenZipOptions = <keyof SevenZipOptions>k;
-            const option = options[key];
-            let sevenZipSwitch: SevenZipSwitch | SevenZipSwitchWithOption | undefined;
-
-            switch (key) {
-                case 'overwriteMode':
-                    sevenZipSwitch = <SevenZipOverwriteOption>option;
-                    break;
-
-                default:
-                    if (options[<keyof SevenZipOptions>key]) {
-                        sevenZipSwitch = <SevenZipSwitchWithOption>{
-                            type: SevenZip.optionDictionary[key],
-                            option: options[<keyof SevenZipOptions>key]
-                        };
-                    }
-            }
-
-            if (sevenZipSwitch) {
-                result.push(SevenZip.stringifySwitch(sevenZipSwitch));
-            }
-        });
-
-        return result.join(' ');
-    }
+    return result.join(' ');
 }
